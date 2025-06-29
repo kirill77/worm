696bf2938a6e73e3b2ee3d22f72c545fe6a43f0c
Kirill Dmitriev <dkirill@nvidia.com>
2025-06-29 13:01:54 -0700

    Added current time to the World class.

--- src/simulation/World.cpp
+++ src/simulation/World.cpp
@@ -3,10 +3,13 @@
 #include "Organism.h"
 
 World::World(std::shared_ptr<Organism> pOrganism)
+    : m_fCurTimeSec(0.0)
 {
     m_pOrganism = pOrganism;
 }
+
 void World::simulateStep(double dt)
 {
+    m_fCurTimeSec += dt;
     m_pOrganism->simulateStep(dt);
 }
--- src/simulation/World.h
+++ src/simulation/World.h
@@ -6,9 +6,12 @@ struct World
 {
     std::shared_ptr<class Medium> m_pMedium;
     std::shared_ptr<struct Organism> m_pOrganism;
+    double m_fCurTimeSec;
 
 public:
     World(std::shared_ptr<Organism> pOrganism);
     void simulateStep(double dt);
+    
+    double getCurrentTime() const { return m_fCurTimeSec; }
 };
 
